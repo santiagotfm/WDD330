9ba20dcc09e19d67ca493e869319e508bf575f15
santiagotfm <santiagotfm28@gmail.com>
2021-04-24 10:32:40 -0600

    week 1 notes

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+    label: "Week1 notes",
+    url: "week1/index.html"
+    },
+];
+
+// get pointer to <ul> element
+let ul = document.querySelector('ul');
+
+links.forEach(
+    link => {
+        ul.innerHTML +=  
+        `
+            <li>
+                <a href="${link.url}">${link.label}</a>
+            </li>
+        `;
+    }
+);
